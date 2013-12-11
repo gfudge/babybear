d4cedf918951f6c86999dc67d7b4fc16d549300d
gfudge <gfudge@build.(none)>
2013-12-11 15:27:31 +0000

    Added youtube url search

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from urlparse import urljoin
 spotifile = "spotifile.txt"
 
 baseUrl = "http://ws.spotify.com/lookup/1/?uri="
-trackID = "spotify:track:5aZCwTIsfqv22p5bewcrgf"
+#trackID = "spotify:track:5aZCwTIsfqv22p5bewcrgf"
 
 def trackLookup(trackID):
 	try:
 		url = baseUrl + trackID
-		openpage=urllib2.urlopen(url)
+		openpage = urllib2.urlopen(url)
 		soup = BeautifulSoup(openpage.read( ))
-		print soup.track.artist.name.string
 		return soup;
 	except: 
 		print "Could not open page"	
@@ -23,15 +22,32 @@ def trackLookup(trackID):
 	
 
 def parseXML(soup):
-	print soup.track.artist.nameTag
+	trackDetails = []
+	trackDetails.append(soup.track.artist.nameTag.string)
+	trackDetails.append(soup.track.nameTag.string)
+	trackDetails.append(soup.track.album.nameTag.string)
+	return trackDetails; 
 
 def readList(spotifile):
 	return [line.strip() for line in open(spotifile)];
+
+def printTrack(trackDetails):
+	print "Artist: " + trackDetails[0]
+	print "Track: " + trackDetails[1]
+	print "Album: " + trackDetails[2] + "\n"
+
+def YTSearch(trackDetails):
+	YTBASEURL = "https://www.youtube.com/results?search_query="
+	searchResult = YTBASEURL + trackDetails[0] + "+" + trackDetails[1]
+	resultPage = urllib2.urlopen(searchResult)
+	print searchResult
 
 #print trackLookup(trackID)
 trackList = readList(spotifile)
 print "Number of tracks: " + str(len(trackList))
 
 for track in trackList:
-	trackLookup(track)
-	#parseXML(track)
+	#trackLookup(track)
+	trackDetails = parseXML(trackLookup(track))
+	printTrack(trackDetails)
+	#YTSearch(trackDetails)
